7143d3909a76ecbf02cc722e6c349bc4d56be530
LongruiDong <lrdong@pku.edu.cn>
2022-10-23 21:36:31 +0800

    first result

--- batcheval.sh
+++ batcheval.sh
@@ -22,29 +22,29 @@ GT_MESH=Datasets/Replica/cull_replica_mesh/office0.ply
 # OUTPUT_FOLDER=output/Replica/office0gt-nodloss-woNsurf-Ni32
 # OUTPUT_FOLDER=output/Replica/office0gt-nodloss-woNsurf-Ni32-ws10-p2k # output/Replica/office0gt-nodloss-woNsurf-Ni32-ws41 # output/Replica/office0gt-nodloss-woNsurf-Ni32-ws10-p2k-f1k5-it1k # output/Replica/office0gt-nodloss-woNsurf-Ni32-p1k-f4k-it4k
 
-OUTPUT_FOLDER=/home/dlr/Project/nice_pl/output/Replica/office0gt-nodloss-80kf-glr-d120k-minlr-guidesample-woffs-exp6
-# output/Replica/office0gt-nodloss1-Ni32-ef25-it1k-resize2 # output/Replica/office0gt-nodloss1-Ni32-ef25-resize2
-# output/Replica/office0gt-nodloss1-Ns32-ef25-guidesamp-resize2-woffs
-# output/Replica/office0gt-nodloss1-Ns32-ef25-it1k-guidesamp-resize2-woffs
-# output/Replica/office0gt-nodloss1-Ns32-ef25-it1k-guidesamp-resize2 # output/Replica/office0gt-nodloss1-Ns32-ef25-guidesamp-resize2
-# /home/dlr/Project1/nice_pl/output/Replica/office0gt-nodloss-woffs-80kf-glr-d120k-minlr-guidesample
-# output/Replica/office0gt-nodloss1-Ni32-ef25 # output/Replica/office0gt-nodloss1-woNsurf-Ni32-f4k-it4k # office0gt-nodloss1-woNsurf-Ni32-ws40 # office0gt-nodloss1-woNsurf-Ni32-ws10-p2k-it1k #office0gt-nodloss1-woNsurf-Ni32 # office0gt-nodloss1-woNsurf-Ni32-p1k-it1k # office0gt-nodloss-woNsurf-Ni32-p1k-it1k # output/Replica/office0gt-nodloss-woNsurf-Ni16-p1k-f4k-it4k # /home/dlr/Project1/nice-slam/output/Replica/office0gt-nodloss-woNsurf-Ni32-p1k-f4k-it4k-mysamp
-# python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/01950_mesh.ply --gt_mesh $GT_MESH -2d -3d
-python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/final_mesh_eval_rec.ply --gt_mesh $GT_MESH -2d -3d
-
-OUTPUT_FOLDER=/home/dlr/Project/nice_pl/output/Replica/office0gt-nodloss-80kf-glr-d120k-minlr-guidesample-woffs-exp7
-# /home/dlr/Project1/nice_pl/output/Replica/office0gt-nodloss-80kf-glr-d120k-minlr-guidesample
-# output/Replica/office0gt-nodloss1-Ni32-ef25-it1k-resize2-woffs # output/Replica/office0gt-nodloss1-Ni32-ef25-resize2-woffs
-python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/final_mesh_eval_rec.ply --gt_mesh $GT_MESH -2d -3d
-
-OUTPUT_FOLDER=/home/dlr/Project/nice_pl/output/Replica/office0gt-nodloss-80kf-glr-d120k-minlr-guidesample-woffs-exp8
-# /home/dlr/Project1/nice_pl/output/Replica/office0gt-nodloss-noshuffle-80kf-glr-d120k-minlr-guidesample-woffs
-# /home/dlr/Project1/nice_pl/output/Replica/office0gt-nodloss-woffs-80kf-glr-d120k-minlr
-# # output/Replica/office0gt-nodloss1-Ns32-ef25-guidesamp # /home/dlr/Project2/nice-slam/output/Replica/office0gt-ndloss1-woNsurf-Ni32-p1k-f4k-it4k-mysamp
-python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/final_mesh_eval_rec.ply --gt_mesh $GT_MESH -2d -3d
-
-# OUTPUT_FOLDER=output/Replica/office0gt-nodepthloss
+# OUTPUT_FOLDER=/home/dlr/Project/nice_pl/output/Replica/office0gt-nodloss-80kf-glr-d120k-minlr-guidesample-woffs-exp6
+# # output/Replica/office0gt-nodloss1-Ni32-ef25-it1k-resize2 # output/Replica/office0gt-nodloss1-Ni32-ef25-resize2
+# # output/Replica/office0gt-nodloss1-Ns32-ef25-guidesamp-resize2-woffs
+# # output/Replica/office0gt-nodloss1-Ns32-ef25-it1k-guidesamp-resize2-woffs
+# # output/Replica/office0gt-nodloss1-Ns32-ef25-it1k-guidesamp-resize2 # output/Replica/office0gt-nodloss1-Ns32-ef25-guidesamp-resize2
+# # /home/dlr/Project1/nice_pl/output/Replica/office0gt-nodloss-woffs-80kf-glr-d120k-minlr-guidesample
+# # output/Replica/office0gt-nodloss1-Ni32-ef25 # output/Replica/office0gt-nodloss1-woNsurf-Ni32-f4k-it4k # office0gt-nodloss1-woNsurf-Ni32-ws40 # office0gt-nodloss1-woNsurf-Ni32-ws10-p2k-it1k #office0gt-nodloss1-woNsurf-Ni32 # office0gt-nodloss1-woNsurf-Ni32-p1k-it1k # office0gt-nodloss-woNsurf-Ni32-p1k-it1k # output/Replica/office0gt-nodloss-woNsurf-Ni16-p1k-f4k-it4k # /home/dlr/Project1/nice-slam/output/Replica/office0gt-nodloss-woNsurf-Ni32-p1k-f4k-it4k-mysamp
+# # python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/01950_mesh.ply --gt_mesh $GT_MESH -2d -3d
 # python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/final_mesh_eval_rec.ply --gt_mesh $GT_MESH -2d -3d
+
+# OUTPUT_FOLDER=/home/dlr/Project/nice_pl/output/Replica/office0gt-nodloss-80kf-glr-d120k-minlr-guidesample-woffs-exp7
+# # /home/dlr/Project1/nice_pl/output/Replica/office0gt-nodloss-80kf-glr-d120k-minlr-guidesample
+# # output/Replica/office0gt-nodloss1-Ni32-ef25-it1k-resize2-woffs # output/Replica/office0gt-nodloss1-Ni32-ef25-resize2-woffs
+# python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/final_mesh_eval_rec.ply --gt_mesh $GT_MESH -2d -3d
+
+# OUTPUT_FOLDER=/home/dlr/Project/nice_pl/output/Replica/office0gt-nodloss-80kf-glr-d120k-minlr-guidesample-woffs-exp8
+# # /home/dlr/Project1/nice_pl/output/Replica/office0gt-nodloss-noshuffle-80kf-glr-d120k-minlr-guidesample-woffs
+# # /home/dlr/Project1/nice_pl/output/Replica/office0gt-nodloss-woffs-80kf-glr-d120k-minlr
+# # # output/Replica/office0gt-nodloss1-Ns32-ef25-guidesamp # /home/dlr/Project2/nice-slam/output/Replica/office0gt-ndloss1-woNsurf-Ni32-p1k-f4k-it4k-mysamp
+# python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/final_mesh_eval_rec.ply --gt_mesh $GT_MESH -2d -3d
+
+OUTPUT_FOLDER=output/Replica_prior/office0gt-nodloss1-resize2
+python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/final_mesh_eval_rec.ply --gt_mesh $GT_MESH -2d -3d
 
 # OUTPUT_FOLDER=output/Replica/office0gt-nodloss-pix5000
 # python src/tools/eval_recon.py --rec_mesh $OUTPUT_FOLDER/mesh/final_mesh_eval_rec.ply --gt_mesh $GT_MESH -2d -3d
