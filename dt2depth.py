fix tumgt; spcd+ coarse kl a reasonable result

dt2depth.py
@@ -166,12 +166,13 @@ def load_traj(gtpath, save=None, firstI = False):
         timestamp = float(i * 1.0/fps) # 根据帧率设置伪时间
         line = lines[i]
         c2w = np.array(list(map(float, line.split()))).reshape(4, 4)
-        #测试首帧归一化
-        if inv_pose is None: # 首帧位姿归I 但是好像只有tum做了归一化处理 why
-            inv_pose = np.linalg.inv(c2w) #T0w
-            c2w = np.eye(4)
-        else:
-            c2w = inv_pose@c2w # T0w Twi = T0i
+        if firstI:
+            #测试首帧归一化
+            if inv_pose is None: # 首帧位姿归I 但是好像只有tum做了归一化处理 why
+                inv_pose = np.linalg.inv(c2w) #T0w
+                c2w = np.eye(4)
+            else:
+                c2w = inv_pose@c2w # T0w Twi = T0i
         # 转为tum
         tq = SE3toQT(c2w)
         ttq = np.concatenate([np.array([timestamp]), tq], 0) #  带上时间戳
@@ -564,6 +565,7 @@ def main(cfg, args, orbmapdir="/home/dlr/Project1/ORB_SLAM2_Enhanced/result"):
     scale = float(s) # float(1./s)
     print('est map should x {}'.format(scale))
 
+    return 0
     # 投影的话还是要有每帧位姿 再跑前面的orbslam 插值 不准 就先只用kf的吧
     kf_orb_pose = copy.deepcopy(estpose)
     print('load orb-mono pose(tum): {} \n size: {}, {}'.format(kftrajfile, kf_orb_pose.shape[0], kf_orb_pose.shape[1]))

src/Mapper.py
@@ -745,8 +745,9 @@ class Mapper(object):
                     loss += sp_factor* self.w_color_loss * sp_color_loss
             
             # 当使用KL loss 时 继续添加
+            kl_loss = 0
             if self.use_KL_loss and (sigma_loss is not None or sigma_loss_sp is not None):
-                kl_loss = 0
+                # kl_loss = 0
                 if sigma_loss is not None:
                     kl_loss += torch.sum(sigma_loss, dim=1).mean() # (B,) -> 平均到一个值了
                     # loss += self.sigma_lambda*kl_loss # 权重求和

src/utils/Renderer.py
@@ -489,7 +489,7 @@ class Renderer(object):
                 sigma_loss = torch.cat(sigma_loss_list, dim=0)
                 if sigma_loss.shape[0] > 0:
                     
-                    gt_none_zero_mask = confidence > 0 # 只管 kpt 的位置
+                    gt_none_zero_mask = gt_depth > 0 # 只管 kpt 的位置 confidence
                     gt_depth_surface = gt_depth[gt_none_zero_mask]
                     weights_valid = weights[gt_none_zero_mask]
                     z_vals_valid = z_vals[gt_none_zero_mask]
@@ -507,7 +507,7 @@ class Renderer(object):
                                                z_vals_valid[k*interval, :].cpu().numpy(),
                                                sigma_loss[k*interval, :].cpu().numpy(),
                                                os.path.join(weight_vis_dir, prefix+f'_ray_%d.png'%k),
-                                               confidence=confidence_valid)
+                                               confidence=confidence_valid[k*interval].cpu().numpy())
                 
                 # 由于前面 是分batch的 不能统一只关注kpt 这里单独渲染kpt 的ray
                 
@@ -571,13 +571,15 @@ class Renderer(object):
         # 再画个 其代表的高斯分布吧
         # err = 0.02 # 暂时以此为方差吧 之后是以实际方差来代替
         err = (1. / confidence) ** 2
+        if confidence == 0.:
+           err = 0.04 
         prior_y = 1./(np.sqrt(2*np.pi*err)) * np.exp(- (z_vals - prior_depth) ** 2 / (2*err))
         axs[0].plot(z_vals, prior_y, c='tab:orange')
         # axs[0].vlines(prior_depth, linestyle='dashed', color='red')
         axs[1].plot(z_vals, sigma_loss) # 画loss
         axs[1].scatter(z_vals, sigma_loss, c='tab:orange', s=2) # 画loss
-        axs[0].set_title('ray weight. prior depth std: {}'.format(np.sqrt(err)))
-        axs[1].set_title('KL loss: {}'.format(np.sum(sigma_loss)))
+        axs[0].set_title('ray weight. prior depth std: {:.4f}'.format(np.sqrt(err)))
+        axs[1].set_title('KL loss: {:.4f}'.format(np.sum(sigma_loss)))
         # axs.set_xticks([])
         # axs.set_yticks([])
         plt.savefig(plotpath, dpi=200)