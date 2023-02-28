a7965cfcdb18895a4558995bc513fe6299f7995c
LongruiDong <lrdong@pku.edu.cn>
2023-02-28 21:27:38 +0800

    lizhi backup

---
+++ srun-rgb-tum1_imap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+
+srun --partition=xr_research --mpi=pmi2 --gres=gpu:1 -n 1 --ntasks-per-node=1 --job-name=tum1imap-rgb-bl --kill-on-bad-exit=1 python -W ignore run.py configs/TUM_RGBD/freiburg1_desk-rgbonly_imap.yaml --imap >log/t1imap-rgbonly.log
+
+
+# srun --partition=xr_research --mpi=pmi2 --gres=gpu:1 -n 1 --ntasks-per-node=1 --job-name=tum2-rgb-bl --kill-on-bad-exit=1 python -W ignore run.py configs/TUM_RGBD/freiburg2_xyz-rgbonly.yaml >log/tum2-rgbonly.log
+
+# srun --partition=xr_research --mpi=pmi2 --gres=gpu:1 -n 1 --ntasks-per-node=1 --job-name=tum3-rgb-bl --kill-on-bad-exit=1 python -W ignore run.py configs/TUM_RGBD/freiburg3_office-rgbonly.yaml >log/tum3-rgbonly.log
